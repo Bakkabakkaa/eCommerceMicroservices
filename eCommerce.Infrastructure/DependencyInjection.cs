93ef4166453835af546aa0f186553d347d3b7531
Art'm <kasko.artem45@gmail.com>
2026-08-12 02:54:51 +0300

    Add dummy users repository

--- eCommerce.Infrastructure/DependencyInjection.cs
+++ eCommerce.Infrastructure/DependencyInjection.cs
@@ -1,3 +1,5 @@
+using eCommerce.Core.RepositoryContracts;
+using eCommerce.Infrastructure.Repositories;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace eCommerce.Infrastructure;
@@ -14,6 +16,7 @@ public static class DependencyInjection
         // TO DO: Add services to the IoC container
         // Infrastructure services often include data access, caching and other
         // low - level components.
+        services.AddSingleton<IUsersRepository, UsersRepository>();
 
         return services;
     }
