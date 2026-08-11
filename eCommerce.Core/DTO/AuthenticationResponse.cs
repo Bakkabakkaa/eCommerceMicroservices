62bc8ad16b6538c85b143a4443daee5c9e995eaa
Art'm <kasko.artem45@gmail.com>
2026-08-12 02:33:52 +0300

    Add users models

---
+++ eCommerce.Core/DTO/AuthenticationResponse.cs
@@ -0,0 +1,9 @@
+namespace eCommerce.Core.DTO;
+
+public record AuthenticationResponse(
+    Guid UserId,
+    string? Email,
+    string? PersonName,
+    string? Gender,
+    string? Token,
+    bool Success);
