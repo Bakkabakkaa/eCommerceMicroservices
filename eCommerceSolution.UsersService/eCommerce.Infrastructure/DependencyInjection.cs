0dcff76159a0633c671929f2bfe4fc9090594239
Art'm <kasko.artem45@gmail.com>
2026-08-16 16:15:50 +0300

    Initial commit: users-service + angular frontend monorepo

---
+++ eCommerceSolution.UsersService/eCommerce.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,26 @@
+using eCommerce.Core.RepositoryContracts;
+using eCommerce.Infrastructure.dbContext;
+using eCommerce.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace eCommerce.Infrastructure;
+
+public static class DependencyInjection
+{
+    /// <summary>
+    /// Extension method to add infrastructure services to the dependency injection container
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        // TO DO: Add services to the IoC container
+        // Infrastructure services often include data access, caching and other
+        // low - level components.
+        services.AddTransient<IUsersRepository, UsersRepository>();
+
+        services.AddTransient<DapperDbContext>();
+
+        return services;
+    }
+}
