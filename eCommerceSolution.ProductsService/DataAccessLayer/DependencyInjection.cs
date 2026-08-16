96bbab6e21808e9e1d1f03aabee7adee59ef40e2
Art'm <kasko.artem45@gmail.com>
2026-08-16 21:13:49 +0300

    Add dbContext

--- eCommerceSolution.ProductsService/DataAccessLayer/DependencyInjection.cs
+++ eCommerceSolution.ProductsService/DataAccessLayer/DependencyInjection.cs
@@ -1,12 +1,20 @@
+using DataAccessLayer.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DataAccessLayer;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddDataAccessLayer(this IServiceCollection services)
+    public static IServiceCollection AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration)
     {
         // TO DO: Add Data Access Layer services into the IoC container
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseMySQL(configuration.GetConnectionString("DefaultConnection")!);
+        });
 
         return services;
     }
--- eCommerceSolution.ProductsService/ProductsMicroservice.API/Program.cs
+++ eCommerceSolution.ProductsService/ProductsMicroservice.API/Program.cs
@@ -5,7 +5,7 @@ using ProductsMicroservice.API.Middlewares;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add DAL and BLL services
-builder.Services.AddDataAccessLayer();
+builder.Services.AddDataAccessLayer(builder.Configuration);
 builder.Services.AddBusinessLogicLayer();
 
 builder.Services.AddControllers();
