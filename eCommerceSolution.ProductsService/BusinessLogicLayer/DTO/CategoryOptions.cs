556abfdd6252e405132eb277e9cb1d28961eaf91
Art'm <kasko.artem45@gmail.com>
2026-08-17 22:47:01 +0300

    Add dtos

---
+++ eCommerceSolution.ProductsService/BusinessLogicLayer/DTO/CategoryOptions.cs
@@ -0,0 +1,9 @@
+namespace BusinessLogicLayer.DTO;
+
+public enum CategoryOptions
+{
+    Electronics,
+    HomeAppliances,
+    Furniture,
+    Accessories
+}
---
+++ eCommerceSolution.ProductsService/BusinessLogicLayer/ServiceContracts/IProductService.cs
@@ -0,0 +1,49 @@
+using System.Linq.Expressions;
+using BusinessLogicLayer.DTO;
+using DataAccessLayer.Entities;
+
+namespace BusinessLogicLayer.ServiceContracts;
+
+public interface IProductService
+{
+    /// <summary>
+    /// Retrieves the list of products from the products repository
+    /// </summary>
+    /// <returns>Returns list of ProductResponse objects</returns>
+    Task<List<ProductResponse?>> GetProducts();
+
+    /// <summary>
+    /// Retrieves list of products matching with given condition
+    /// </summary>
+    /// <param name="conditionExpression">Expressions that represents condition to check</param>
+    /// <returns>Returns matching products</returns>
+    Task<List<ProductResponse?>> GetProductsByCondition(Expression<Func<ProductResponse, bool>> conditionExpression);
+
+    /// <summary>
+    /// Returns a single product that matches with given condition
+    /// </summary>
+    /// <param name="conditionExpression">Express that represents the condition to check</param>
+    /// <returns>Returns matching product or null</returns>
+    Task<ProductResponse?> GetProductByCondition(Expression<Func<Product, bool>> conditionExpression);
+
+    /// <summary>
+    /// Adds (inserts) product into the table using products repository
+    /// </summary>
+    /// <param name="productAddRequest">Product to insert</param>
+    /// <returns>Product after inserting or null if unsuccessful</returns>
+    Task<ProductResponse?> AddProduct(ProductAddRequest productAddRequest);
+
+    /// <summary>
+    /// Updates the existing product based on the ProductID
+    /// </summary>
+    /// <param name="productUpdateRequest">Product data to update</param>
+    /// <returns>Returns product object after successful update, otherwise null</returns>
+    Task<ProductResponse?> UpdateProduct(ProductUpdateRequest productUpdateRequest);
+
+    /// <summary>
+    /// Deletes an existing product based on given product id
+    /// </summary>
+    /// <param name="productID">ProductID to search and delete</param>
+    /// <returns>Returns true if the deletion is successful, otherwise false</returns>
+    Task<bool> DeleteProduct(Guid productID);
+}
