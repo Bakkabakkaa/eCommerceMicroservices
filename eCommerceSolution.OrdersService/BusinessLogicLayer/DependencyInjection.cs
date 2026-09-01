2bb33eb5216c6478c3a8d3922c18f4614e2c5bf4
Art'm <kasko.artem45@gmail.com>
2026-09-02 02:38:23 +0300

    add services and use middlewares to Program.cs

--- eCommerceSolution.OrdersService/BusinessLogicLayer/DependencyInjection.cs
+++ eCommerceSolution.OrdersService/BusinessLogicLayer/DependencyInjection.cs
@@ -1,10 +1,11 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BusinessLogicLayer;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddBusinessLogicLayer(IServiceCollection services)
+    public static IServiceCollection AddBusinessLogicLayer(this IServiceCollection services, IConfiguration configuration)
     {
         return services;
     }
--- eCommerceSolution.OrdersService/OrdersMicroservice.API/Program.cs
+++ eCommerceSolution.OrdersService/OrdersMicroservice.API/Program.cs
@@ -1,6 +1,51 @@
+using DataAccessLayer;
+using BusinessLogicLayer;
+using FluentValidation.AspNetCore;
+using OrdersMicroservice.API.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add DAL and BLL services
+builder.Services.AddDataAccessLayer(builder.Configuration);
+builder.Services.AddBusinessLogicLayer(builder.Configuration);
+
+builder.Services.AddControllers();
+
+// FluentValidation
+builder.Services.AddFluentValidationAutoValidation();
+
+// Swagger
+builder.Services.AddSwaggerGen();
+
+// Cors
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("http://localhost:4200")
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseExceptionHandlingMiddleware();
+app.UseRouting();
+
+// Cors
+app.UseCors();
+
+// Swagger
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// Auth
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Endpoints
+app.MapControllers();
 
 app.Run();
