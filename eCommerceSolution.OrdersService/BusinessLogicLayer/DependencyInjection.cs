d8c8cd187192127cd2c20da0c9403b361b56cbaa
Art'm <kasko.artem45@gmail.com>
2026-09-02 02:16:49 +0300

    add DependencyInjection static classes

---
+++ eCommerceSolution.OrdersService/BusinessLogicLayer/DependencyInjection.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BusinessLogicLayer;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddBusinessLogicLayer(IServiceCollection services)
+    {
+        return services;
+    }
+}
